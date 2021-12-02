18ac4676517ac275720b9a371e2f94acaf9e8131
feresTalhaoui <firas.talhaoui@gmail.com>
2021-12-02 16:13:45 +0100

    add:news pagination, fix:width and margins

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -9,42 +9,92 @@ import { HttpServiceService } from 'src/app/services/http-service.service';
 export class HomeComponent implements OnInit {
 usersList=[]
 usersListPage=[]
-activePage=[]
-todoList=[]
+userActivePage=[]
+newsList=[]
+newsListPage=[]
+newsactivePage=[]
 numberOfUserPages:number;
-
+numberOfNewsPages:number
+newsNumberPages=[]
 numberPages=[];
   constructor(private _httpServiceService:HttpServiceService) { }
 
   ngOnInit(): void {
     this._httpServiceService.getUsers().subscribe(res=>{
       this.usersList=res['data']
-      this.initializePages()
+      this.initializeUserPages()
     })
     this._httpServiceService.getTodo().subscribe(res=>{
-      this.todoList=res['data']
-      this.todoList.filter(el=>el.status=='completed')
+      this.newsList=res['data']
+      this.newsList.filter(el=>el.status=='completed')
+      this.initializeNewsPages()
     })
   }
-  changePage(index){
+  changeUserPage(index){
     let firstItem=(index-1)*6;
     this.usersListPage=this.usersList.slice(firstItem,index*6)
-    this.activePage.fill(false)
-    this.activePage[index-1]=true
-    console.log( this.activePage)
+    this.userActivePage.fill(false)
+    this.userActivePage[index-1]=true
+    console.log( this.userActivePage)
     console.log('index',index)
   }
-  initializePages(){
-    this.usersListPage=Object.assign(this.usersList.slice(0,5))
+  initializeUserPages(){
+    this.usersListPage=Object.assign(this.usersList.slice(0,6));
+    
+    this.getUserPageNumber();
+    this.initializeUserPageArray();
+    this.initializeActivePageArray();
+
+
+  }
+  initializeNewsPages(){
+this.newsListPage=Object.assign(this.newsList.slice(0,3))
+this.getNewsPageNumber();
+this.initializeNewsPageArray();
+this.initializeActiveNewsPageArray()
+
+  }
+  getNewsPageNumber(){
+this.numberOfNewsPages=this.newsList.length/3;
+this.numberOfNewsPages=Math.floor(this.numberOfNewsPages)
+this.newsList.length%3>0?this.numberOfNewsPages++:true;
+console.log(this.numberOfNewsPages)
+  }
+  initializeActiveNewsPageArray(){
+    this.newsactivePage=Array(this.numberOfNewsPages).fill(false);
+      this.newsactivePage[0]=true
+      console.log(this.newsactivePage)
+  }
+  initializeNewsPageArray(){
+    this.newsNumberPages=Array(this.numberOfNewsPages).fill(0)
+      for(let index=0;index<this.numberOfNewsPages;index++){
+        this.newsNumberPages[index]=index+1
+      }
+      console.log(this.newsNumberPages)
+  }
+
+  getUserPageNumber(){
     this.numberOfUserPages=this.usersList.length/6;
     this.numberOfUserPages=Math.floor(this.numberOfUserPages);
     (this.usersList.length%6)>0?this.numberOfUserPages++:true;
-    this.numberPages=Array(this.numberOfUserPages).fill(0)
-    this.activePage=Array(this.numberOfUserPages).fill(false);
-    this.activePage[0]=true
+  }
+initializeActivePageArray(){
+  this.userActivePage=Array(this.numberOfUserPages).fill(false);
+    this.userActivePage[0]=true
+}
+initializeUserPageArray(){
+  this.numberPages=Array(this.numberOfUserPages).fill(0)
     for(let index=0;index<this.numberOfUserPages;index++){
       this.numberPages[index]=index+1
     }
-  }
+}
 
+changeNewsPage(index){
+  let firstItem=(index-1)*3;
+  this.newsListPage=this.newsList.slice(firstItem,index*3)
+  this.newsactivePage.fill(false)
+  this.newsactivePage[index-1]=true
+  console.log( this.newsactivePage)
+  console.log('index',index)
+}
 }
